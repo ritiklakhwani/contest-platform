b21857cb4f3f3a331678fed1dc7bc8c583cb325f
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-02-21 22:42:29 +0530

    feat: done with the get contest details endpoint

--- src/contest.ts
+++ src/contest.ts
@@ -10,10 +10,16 @@ import {
   apiLimiter,
   submissionLimiter,
 } from "./middleware";
+import { id } from "zod/locales";
 const router = Router();
 
 //create contest
-router.post("/create", authMiddleware, creatorAuth, apiLimiter, async (req: Request, res: Response) => {
+router.post(
+  "/create",
+  authMiddleware,
+  creatorAuth,
+  apiLimiter,
+  async (req: Request, res: Response) => {
     try {
       const parsed = createContestSchema.safeParse(req.body);
       if (!parsed.success) {
@@ -54,7 +60,82 @@ router.post("/create", authMiddleware, creatorAuth, apiLimiter, async (req: Requ
   },
 );
 
+router.get(
+  "/:contestId",
+  authMiddleware,
+  apiLimiter,
+  async (req: Request, res: Response) => {
+    try {
+      if (typeof req.params.contestId !== "string")
+        return errorResponse(res, "Invalid contest ID", 400);
+      const contestId = parseInt(req.params.contestId);
+      const contest = await prisma.contest.findUnique({
+        where: { id: contestId },
+        include: {
+          mcqs: true,
+          dsaProblems: true,
+        },
+      });
 
+      if (!contest) return errorResponse(res, "Contest not found", 404);
 
+      const isCreator = req.user!.id === contest.creator_id;
+
+      const formatMcqs = (mcqs: any) => {
+        const base = {
+          id: mcqs.id,
+          questionText: mcqs.question_text,
+          options: mcqs.options,
+          points: mcqs.points,
+        };
+        if (isCreator)
+          return { ...base, correctOptionIndex: mcqs.correct_option_index };
+        return base;
+      };
+
+      const formatDsaProblems = (problem: any) => {
+        const base = {
+          id: problem.id,
+          title: problem.title,
+          description: problem.description,
+          tags: problem.tags,
+          points: problem.points,
+          timeLimit: problem.time_limit,
+          memoryLimit: problem.memory_limit,
+        };
+        if (isCreator) return { ...base, testCases: problem.test_cases };
+        return base;
+      };
+
+      res.json(
+        successResponse(
+          res,
+          {
+            id: contest.id,
+            title: contest.title,
+            description: contest.description,
+            startTime: contest.start_time,
+            endTime: contest.end_time,
+            creatorId: contest.creator_id,
+            mcqs: contest.mcqs.map(formatMcqs),
+            dsaProblems: contest.dsaProblems.map(formatDsaProblems),
+          },
+          200,
+        ),
+      );
+    } catch (error) {
+      console.error("Get contest error:", error);
+      errorResponse(res, "Failed to fetch contest details", 500);
+    }
+  },
+);
+
+router.post('/:contestId/mcq', authMiddleware, creatorAuth, apiLimiter, async (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+})
 
 export default router;
