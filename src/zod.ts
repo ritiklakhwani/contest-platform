e212e29e5985e6b9c4adf3bcb4d1ecf0b54cc6a3
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-02-24 03:40:49 +0530

    fix: added default role functionality now by default role will be contestee if user doesnt enter role

--- src/zod.ts
+++ src/zod.ts
@@ -5,7 +5,7 @@ export const signupSchema = z.object({
   name: z.string().min(1, "Name is required"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
-  role: z.enum(["creator", "contestee"]),
+  role: z.enum(["creator", "contestee"]).optional().default('contestee'),
 });
 
 // login schema
